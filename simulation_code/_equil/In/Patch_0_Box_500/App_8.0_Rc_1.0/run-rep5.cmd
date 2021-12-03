ce8f69db32c40b3ae505c8bfd9d8f7d7f6950cd5
Chenyi Fei <cfei@princeton.edu>
2021-12-03 12:48:45 -0500

    Add files via upload

---
+++ simulation_code/_equil/In/Patch_0_Box_500/App_8.0_Rc_1.0/run-rep5.cmd
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J P0_B500_A8.0_R1.0_#5
+#SBATCH -o ./Report/slurm-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=23:30:00
+
+module purge
+module load intel/18.0/64/18.0.3.222 intel-mpi/intel/2018.3/64
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+srun $HOME/.local/bin/lmp_della -sf intel -in Runner5.in
