e13abc1ccd7127701bc38ed842e90274520c5aa1
Chenyi Fei <cfei@princeton.edu>
2021-12-03 10:28:53 -0500

    Delete div.m

--- image_analysis/div.m
+++
@@ -1,7 +0,0 @@
-% Divergence of (u1, u2) on domain [-Lx, Lx]*[-Ly, Ly]
-function uu = div(u1, u2, dx, dy)    
-    [u1x, ~]  = grad(u1,dx,dy);
-    [~, u2y]  = grad(u2,dx,dy);
-
-    uu = u1x + u2y;
-end
